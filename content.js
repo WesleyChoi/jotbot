4a7ba1b0eccc0e78ef5e07de31661c2cb7d173f3
Wesley <wesleyhchoi@gmail.com>
2020-05-29 11:34:10 -0700

    kind of working but not really

--- content.js
+++ content.js
@@ -1,6 +1,15 @@
-alert('Testing!')
+// alert('Testing!')
 
-/* // this is the content js portion of the file, if the alert functions (meaning that the manifest.json finally works) just uncomment this part
+
+// calling the screenshot function
+
+chrome.runtime.onMessage.addListener(function() {
+
+}
+
+
+
+/* Tesseract portion of the content.js script.
 
 <script src='https://unpkg.com/tesseract.js@2.0.2/dist/tesseract.min.js'></script>
 
