1053563db19436430be150bf816ca5d2dd73aa33
Wesley <wesleyhchoi@gmail.com>
2020-05-29 10:30:01 -0700

    working chrome extension, added content files

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
 alert('Testing!')
+
+/* // this is the content js portion of the file, if the alert functions (meaning that the manifest.json finally works) just uncomment this part
+
+<script src='https://unpkg.com/tesseract.js@2.0.2/dist/tesseract.min.js'></script>
+
+
+
+*/
